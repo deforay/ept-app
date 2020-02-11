fe29b308e675b82e71ce3e4f10d3bd314b5ce2ac
Indhumathi <indhumathi@deforay.com>
2020-02-11 16:50:46 +0530

    participant details UI done

--- src/app/dts-hiv-viralload/dts-hiv-viralload.module.ts
+++ src/app/dts-hiv-viralload/dts-hiv-viralload.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { DtsHivViralloadPageRoutingModule } from './dts-hiv-viralload-routing.module';
 
 import { DtsHivViralloadPage } from './dts-hiv-viralload.page';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    MaterialModule,
     DtsHivViralloadPageRoutingModule
   ],
   declarations: [DtsHivViralloadPage]
--- src/app/dts-hiv-viralload/dts-hiv-viralload.page.ts
+++ src/app/dts-hiv-viralload/dts-hiv-viralload.page.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators
+} from '@angular/forms';
+
+import {
+  Router
+} from '@angular/router';
+import {
+  ErrorStateMatcher
+} from '@angular/material/core';
+/** Error when invalid control is dirty, touched, or submitted. */
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-dts-hiv-viralload',
@@ -7,9 +31,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DtsHivViralloadPage implements OnInit {
 
-  constructor() { }
+  panelOpenState = false;
+  partDetailsArray = [];
+  tiles: Tile[] = [{
+      text: 'One',
+      cols: 1,
+      rows: 1,
+      color: 'lightblue'
+    },
+    {
+      text: 'Two',
+      cols: 1,
+      rows: 1,
+      color: 'lightgreen'
+    },
+    // {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    // {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+  ];
 
-  ngOnInit() {
+  constructor() {
+    this.partDetailsArray = [{
+        "key": "Participant Name",
+        "value": "Indhu"
+      },
+      {
+        "key": "Participant Code",
+        "value": "4125"
+      },
+      {
+        "key": "Affiliation",
+        "value": "Laboratory"
+      },
+      {
+        "key": "Tel Phone No",
+        "value": "044-345444645"
+      },
+      {
+        "key": "Mobile No",
+        "value": "9841119818"
+      }
+    ];
+ 
   }
 
+
+
+  ngOnInit() {}
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+}
+
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
 }
