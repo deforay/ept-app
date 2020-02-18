b6b27068026c7a42ecf6c0f60020a3b937c7579c
Indhumathi <indhumathi@deforay.com>
2020-02-18 20:50:28 +0530

    viral load shipment details binded

--- src/app/dts-hiv-viralload/dts-hiv-viralload.page.ts
+++ src/app/dts-hiv-viralload/dts-hiv-viralload.page.ts
@@ -72,7 +72,14 @@ export class DtsHivViralloadPage implements OnInit {
   othervlassay;
   specVolTest;
   assayExpDate;
-  qcRadioArray =[];
+  qcRadioArray = [];
+  modeOfReceiptArray = [];
+  receiptmode: any;
+  selectedModeOfReceipt: any = [];
+  selectedVlAssay=[];
+  vlassay:any;
+  isSelectedOther:boolean=false;
+  changedVlAssay=[];
   constructor(private activatedRoute: ActivatedRoute, private storage: Storage, public ToastService: ToastService,
     public LoaderService: LoaderService, public CrudServiceService: CrudServiceService) {
 
@@ -102,24 +109,37 @@ export class DtsHivViralloadPage implements OnInit {
           if (this.shipmentsDetailsArray['testDate']) {
             this.testDate = new Date(this.shipmentsDetailsArray['testDate']);
           }
-          if(this.shipmentsDetailsArray['assayExpirationDate']){
+          if (this.shipmentsDetailsArray['assayExpirationDate']) {
             this.assayExpDate = new Date(this.shipmentsDetailsArray['assayExpirationDate']);
           }
-          if(this.shipmentsDetailsArray['specimenVolume']){
+          if (this.shipmentsDetailsArray['specimenVolume']) {
             this.specVolTest = this.shipmentsDetailsArray['specimenVolume'];
           }
-          if(this.shipmentsDetailsArray['qcData'].status==true){
+          if (this.shipmentsDetailsArray['qcData'].status == true) {
             this.qcRadioArray = this.shipmentsDetailsArray['qcData'].qcRadio;
           }
-          
-          
-          // if (this.shipmentsDetailsArray['vlAssaySelect']) {
-          //   this.vlAssayArray = this.shipmentsDetailsArray['vlAssaySelect'];
-          // }
+          if(this.shipmentsDetailsArray['modeOfReceiptSelect']) {
+            this.modeOfReceiptArray = this.shipmentsDetailsArray['modeOfReceiptSelect'];
+            this.selectedModeOfReceipt = this.modeOfReceiptArray.filter(
+              modeOfRec => modeOfRec.selected == "selected");
+            this.receiptmode = this.selectedModeOfReceipt[0].value;
+          }
+          if(this.shipmentsDetailsArray['vlAssaySelect']) {
+            this.isSelectedOther=false;
+            this.selectedVlAssay = this.shipmentsDetailsArray['vlAssaySelect'].filter(
+              vlAssay => vlAssay.selected == "selected");
+            this.vlassay = this.selectedVlAssay[0].value;
+            if(this.shipmentsDetailsArray['otherAssay']){
+              this.othervlassay=this.shipmentsDetailsArray['otherAssay'];
+              if(this.selectedVlAssay[0].selected=='selected'){
+                this.isSelectedOther=true;
+              }
+            }
+          }
         }
         if (vlDataObj[0].vlData.Heading3.status == true) {
           this.ptPanelTestArray = vlDataObj[0].vlData.Heading3.data;
-          
+
         }
         if (vlDataObj[0].vlData.Heading4.status == true) {
           this.otherInfoArray = vlDataObj[0].vlData.Heading4.data;
@@ -149,6 +169,19 @@ export class DtsHivViralloadPage implements OnInit {
     } else {
       this.vlLog = (Math.log10(this.vlCalc)).toFixed(1);
 
+    }
+  }
+
+  changeViralLoadAssay(vl){
+    if(vl){
+      this.changedVlAssay = this.shipmentsDetailsArray['vlAssaySelect'].filter(
+        vlAssayItem => vlAssayItem.value ==vl && vlAssayItem.show=='Other');
+    }
+    if(this.changedVlAssay.length!=0){
+      this.isSelectedOther=true;
+    }
+    else{
+      this.isSelectedOther=false;
     }
   }
 }
