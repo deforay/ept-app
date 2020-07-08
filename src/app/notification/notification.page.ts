7eab662cf391c40e51d3237b742916b751c599e1
Indhumathi <indhumathi@deforay.com>
2020-07-08 21:00:49 +0530

    API integrated

--- src/app/notification/notification.page.ts
+++ src/app/notification/notification.page.ts
@@ -1,28 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  CrudServiceService
+} from '../../app/service/crud/crud-service.service';
+import {
+  AlertService
+} from '../../app/service/providers';
+import {
+  Storage
+} from '@ionic/storage';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.page.html',
   styleUrls: ['./notification.page.scss'],
 })
 export class NotificationPage implements OnInit {
-  notificationsArray=[];
-  constructor() {
-    this.notificationsArray=
-    [
-      {
-        "content":"Your dbs -serology test result has came. Please download the result in the individual report page"
+
+  notificationsArray = [];
+  appVersionNumber: any;
+  authToken: any;
+  pushNotificationToken:any;
+  constructor(public CrudServiceService: CrudServiceService,
+    public alertService: AlertService, private storage: Storage, ) {
+    this.notificationsArray = [{
+        "content": "Your dbs -serology test result has came. Please download the result in the individual report page"
       },
       {
-        "content":"new shipment DTS004355 came. Please submit the serology response within the result due date.And once our evaluation will start."
+        "content": "new shipment DTS004355 came. Please submit the serology response within the result due date.And once our evaluation will start."
       },
       {
-        "content":"Thank u for your response."
+        "content": "Thank u for your response."
       }
     ]
-   }
-
-  ngOnInit() {
+  
   }
 
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.onLoadNotifications();
+  }
+
+  onLoadNotifications() {
+    this.storage.get('appVersionNumber').then((appVersionNumber) => {
+      if (appVersionNumber) {
+        this.appVersionNumber = appVersionNumber;
+      }
+    })
+    this.storage.get('participantLogin').then((partiLoginResult) => {
+      if (partiLoginResult.authToken) {
+    this.CrudServiceService.getData('/api/participant/get-notifications/?appVersion=' +this.appVersionNumber  + '&authToken=' + partiLoginResult.authToken)
+      .then(result => {
+  
+        console.log(result);
+        if (result["status"] == 'success') {
+          this.notificationsArray = result['data'];
+          
+        }
+      }, (err) => {
+        this.alertService.presentAlert('Alert', 'Something went wrong.Please try again later');
+      });
+  }
+})
+  }
 }
